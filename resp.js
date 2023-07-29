b122b42c1c51caeb9cd005930de16e4bc3118a13
imran-83 <workwithimranahmed@gmail.com>
2023-07-29 18:52:56 +0530

    ft1

---
+++ resp.js
@@ -0,0 +1,10 @@
+burger =  document.querySelector('.burger')
+navbar =  document.querySelector('.navbar')
+navlist =  document.querySelector('.navlist')
+
+
+
+burger.addEventListener('click', ()=>{
+    navbar.classList.toggle('h-nav');
+    navlist.classList.toggle('v-class');
+})
